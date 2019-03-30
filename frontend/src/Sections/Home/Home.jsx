4e071025371d40317d0b3ec79d41184a9438622c
Michael Gates <michaelgatesdev@gmail.com>
2019-03-30 11:35:45 -0400

    more frontend stuff

---
+++ frontend/src/Sections/Home/Home.jsx
@@ -0,0 +1,38 @@
+
+import React, { Component, Fragment } from 'react';
+import './Home.css';
+
+// import NavBar from "../../Components/NavBar/NavBar";
+
+class Room extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true
+        };
+    }
+
+    componentDidMount() {
+    }
+
+
+    render() {
+
+        if (this.state.loading) {
+            // return splashscreen
+            return <p>Loading...</p>
+        }
+
+
+        return (
+            <Fragment>
+                <section className="container" id="home-section">
+                </section>
+            </Fragment>
+        );
+    }
+}
+
+export default Room;
