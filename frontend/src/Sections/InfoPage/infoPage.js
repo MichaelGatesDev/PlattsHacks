6cefeb43b7f720abe8f91c6619ddd3ab76e2058f
Keith Prevost <rickyprev@yahoo.com>
2019-03-30 11:38:45 -0400

    start project

---
+++ frontend/src/Sections/InfoPage/infoPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./infoPageS.css";
+
+const InfoPage = () => {
+  return (
+    <div className="infoContainer">
+      <div className="nameDisplay" />
+    </div>
+  );
+};
+
+export default InfoPage;
